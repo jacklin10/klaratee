014149279e0eeb0e29fa1c2ccdadd9bea6874554
jacklin <jacklin10@gmail.com>
2012-02-27 22:58:28 -0500

    Stopped ignoring the vendor files

--- vendor/plugins/attachment_fu/init.rb
+++ vendor/plugins/attachment_fu/init.rb
@@ -4,6 +4,9 @@ Tempfile.class_eval do
   # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
   def make_tmpname(basename, n)
     ext = nil
+    # bug when using certain versions of ruby.  my 1.9.2@290 worked while rpgs @280 showed the error.
+    # @see http://tinyw.in/XbfH
+    n ||= 0
     sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
   end
 end
